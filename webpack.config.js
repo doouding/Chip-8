2595b68914f3915d20946d8ddd8ca5f6fa46f9f3
codert <codert.sn@gmail.com>
2020-06-07 05:00:20 +0800

    try to add demo page

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
         extensions: ['.ts', '.tsx', '.js']
     },
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'docs'),
         filename: '[name].js',
         globalObject: 'window'
     },
